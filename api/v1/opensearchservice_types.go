f1c95d19d7fbae1fb6af3d907a9486060be93b83
Artem Lapin <artem.lapin@netcracker.com>
2022-12-07 12:23:06 +0300

    fix

--- api/v1/opensearchservice_types.go
+++ api/v1/opensearchservice_types.go
@@ -27,7 +27,7 @@ type S3 struct {
 	BasePath        string `json:"basePath,omitempty"`
 	Region          string `json:"region,omitempty"`
 	SecretName      string `json:"secretName,omitempty"`
-	GCSEnabled      bool   `json:"GCSEnabled,omitempty"`
+	GcsEnabled      bool   `json:"gcsEnabled,omitempty"`
 }
 
 // Dashboards structure defines parameters necessary for interaction with Dashboards
