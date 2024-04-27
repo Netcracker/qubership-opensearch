861fc7cc83c77d8cbf5b51e2bb5bd521672aef6a
Dmitrii Kulikov <dmitrii.kulikov@netcracker.com>
2024-04-27 11:17:36 +0300

    Use nc-schema-gen

--- dev-kit/schema/make_json_schema.sh
+++ dev-kit/schema/make_json_schema.sh
@@ -1,6 +1,9 @@
-python schema_filler.py --leveling --skip-type-mismatch --off-array-list-mismatch-log \
+# Refer to https://git.netcracker.com/Personal.Streaming.Platform/values-schema-generator#user-guide
+# to install nc-schema-gen
+ROOT_PATH=../..
+nc-schema-gen --leveling --skip-type-mismatch --off-array-list-mismatch-log \
   -n opensearch-service \
-  -d ../docs/public/installation.md \
-  -v ../charts/helm/opensearch-service/values.yaml \
-  -o ../charts/helm/opensearch-service/values.schema.json \
-  -r ../charts/helm/opensearch-service/values.rules.json
+  -d $ROOT_PATH/docs/public/installation.md \
+  -v $ROOT_PATH/charts/helm/opensearch-service/values.yaml \
+  -o $ROOT_PATH/charts/helm/opensearch-service/values.schema.json \
+  -r $ROOT_PATH/charts/helm/opensearch-service/values.rules.json
