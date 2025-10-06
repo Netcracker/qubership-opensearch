b783052422df37086e5314c39ae2333e62abd251
Altynur Tynyshbekov <alnurt00@gmail.com>
2025-10-06 18:23:29 +0500

    fix: fix linter's errors

--- docker-opensearch/2x/docker/docker-entrypoint.sh
+++ docker-opensearch/2x/docker/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-${OPENSEARCH_HOME}/bin/opensearch-plugin install \
-  --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-s3/repository-s3-2.19.3.zip"
-${OPENSEARCH_HOME}/bin/opensearch-plugin install \
-  --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-gcs/repository-gcs-2.19.3.zip"
-${OPENSEARCH_HOME}/bin/opensearch-plugin install \
-  --batch --verbose "file://${OPENSEARCH_HOME}/dist/opensearch-filter-plugin/opensearch-filter-plugin-2.19.3.0.zip"
-rm -rf ${OPENSEARCH_HOME}/dist
+"${OPENSEARCH_HOME}"/bin/opensearch-plugin install \
+    --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-s3/repository-s3-2.19.3.zip"
+"${OPENSEARCH_HOME}"/bin/opensearch-plugin install \
+    --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-gcs/repository-gcs-2.19.3.zip"
+"${OPENSEARCH_HOME}"/bin/opensearch-plugin install \
+    --batch --verbose "file://${OPENSEARCH_HOME}/dist/opensearch-filter-plugin/opensearch-filter-plugin-2.19.3.0.zip"
+rm -rf "${OPENSEARCH_HOME}"/dist
 
 if [[ -n "$OPENSEARCH_SECURITY_CONFIG_PATH" ]]; then
-  # Set internal users
-  password=$("${OPENSEARCH_HOME}/plugins/opensearch-security/tools/hash.sh" -p "${OPENSEARCH_PASSWORD}" | grep -v "\*\*")
-  cat >"${OPENSEARCH_SECURITY_CONFIG_PATH}/internal_users.yml" <<EOF
+    # Set internal users
+    password=$("${OPENSEARCH_HOME}/plugins/opensearch-security/tools/hash.sh" -p "${OPENSEARCH_PASSWORD}" | grep -v "\*\*")
+    cat >"${OPENSEARCH_SECURITY_CONFIG_PATH}/internal_users.yml" <<EOF
 _meta:
   type: "internalusers"
   config_version: 2
@@ -39,24 +39,24 @@ DESTINATION_KEYSTORE_PATH=/usr/share/opensearch/config/cacerts
 KEYSTORE_PATH=${JAVA_HOME}/lib/security/cacerts
 
 echo "Copy Java cacerts to $DESTINATION_KEYSTORE_PATH"
-${JAVA_HOME}/bin/keytool --importkeystore -noprompt \
-        -srckeystore $KEYSTORE_PATH \
+"${JAVA_HOME}"/bin/keytool --importkeystore -noprompt \
+        -srckeystore "$KEYSTORE_PATH" \
         -srcstorepass changeit \
         -destkeystore $DESTINATION_KEYSTORE_PATH \
         -deststorepass changeit &> /dev/null
 
 if [[ "$(ls $PUBLIC_CERTS_DIR)" ]]; then
-    for filename in $PUBLIC_CERTS_DIR/*; do
+    for filename in "$PUBLIC_CERTS_DIR"/*; do
         echo "Import $filename certificate to Java cacerts"
-        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias $filename -file $filename
+        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias "$filename" -file "$filename"
     done;
 fi
 
 if [[ "$(ls $S3_CERTS_DIR)" ]]; then
-    for filename in $S3_CERTS_DIR/*; do
+    for filename in "$S3_CERTS_DIR"/*; do
         echo "Import $filename certificate to Java cacerts"
-        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias $filename -file $filename
-        keytool -import -trustcacerts -keystore $KEYSTORE_PATH -storepass changeit -noprompt -alias $filename -file $filename
+        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias "$filename" -file "$filename"
+        keytool -import -trustcacerts -keystore $KEYSTORE_PATH -storepass changeit -noprompt -alias "$filename" -file "$filename"
     done;
 fi
 
--- docker-opensearch/3x/docker/docker-entrypoint.sh
+++ docker-opensearch/3x/docker/docker-entrypoint.sh
@@ -2,29 +2,29 @@
 
 set -e
 
-${OPENSEARCH_HOME}/bin/opensearch-plugin install \
-  --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-s3/repository-s3-3.2.0.zip"
-${OPENSEARCH_HOME}/bin/opensearch-plugin install \
-  --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-gcs/repository-gcs-3.2.0.zip"
-${OPENSEARCH_HOME}/bin/opensearch-plugin install \
-  --batch --verbose "file://${OPENSEARCH_HOME}/dist/opensearch-filter-plugin/opensearch-filter-plugin-3.2.0.0.zip"
-rm -rf ${OPENSEARCH_HOME}/dist
+"${OPENSEARCH_HOME}"/bin/opensearch-plugin install \
+    --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-s3/repository-s3-3.2.0.zip"
+"${OPENSEARCH_HOME}"/bin/opensearch-plugin install \
+    --batch --verbose "file://${OPENSEARCH_HOME}/dist/repository-gcs/repository-gcs-3.2.0.zip"
+"${OPENSEARCH_HOME}"/bin/opensearch-plugin install \
+    --batch --verbose "file://${OPENSEARCH_HOME}/dist/opensearch-filter-plugin/opensearch-filter-plugin-3.2.0.0.zip"
+rm -rf "${OPENSEARCH_HOME}"/dist
 
 if [[ -n "$OPENSEARCH_SECURITY_CONFIG_PATH" ]]; then
-  # Set internal users
-  password=$("${OPENSEARCH_HOME}/plugins/opensearch-security/tools/hash.sh" -p "${OPENSEARCH_PASSWORD}" | grep -v "\*\*")
-  cat >"${OPENSEARCH_SECURITY_CONFIG_PATH}/internal_users.yml" <<EOF
+    # Set internal users
+    password=$("${OPENSEARCH_HOME}/plugins/opensearch-security/tools/hash.sh" -p "${OPENSEARCH_PASSWORD}" | grep -v "\*\*")
+    cat >"${OPENSEARCH_SECURITY_CONFIG_PATH}/internal_users.yml" <<EOF
 _meta:
-  type: "internalusers"
-  config_version: 2
+    type: "internalusers"
+    config_version: 2
 
 # Define your internal users here
 ${OPENSEARCH_USERNAME}:
-  hash: "${password}"
-  reserved: false
-  backend_roles:
-  - "admin"
-  description: "Admin user"
+    hash: "${password}"
+    reserved: false
+    backend_roles:
+    - "admin"
+    description: "Admin user"
 EOF
 fi
 
@@ -39,24 +39,24 @@ DESTINATION_KEYSTORE_PATH=/usr/share/opensearch/config/cacerts
 KEYSTORE_PATH=${JAVA_HOME}/lib/security/cacerts
 
 echo "Copy Java cacerts to $DESTINATION_KEYSTORE_PATH"
-${JAVA_HOME}/bin/keytool --importkeystore -noprompt \
-        -srckeystore $KEYSTORE_PATH \
+"${JAVA_HOME}"/bin/keytool --importkeystore -noprompt \
+        -srckeystore "$KEYSTORE_PATH" \
         -srcstorepass changeit \
-        -destkeystore $DESTINATION_KEYSTORE_PATH \
+        -destkeystore "$DESTINATION_KEYSTORE_PATH" \
         -deststorepass changeit &> /dev/null
 
 if [[ "$(ls $PUBLIC_CERTS_DIR)" ]]; then
     for filename in $PUBLIC_CERTS_DIR/*; do
         echo "Import $filename certificate to Java cacerts"
-        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias $filename -file $filename
+        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias "$filename" -file "$filename"
     done;
 fi
 
 if [[ "$(ls $S3_CERTS_DIR)" ]]; then
     for filename in $S3_CERTS_DIR/*; do
         echo "Import $filename certificate to Java cacerts"
-        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias $filename -file $filename
-        keytool -import -trustcacerts -keystore $KEYSTORE_PATH -storepass changeit -noprompt -alias $filename -file $filename
+        keytool -import -trustcacerts -keystore $DESTINATION_KEYSTORE_PATH -storepass changeit -noprompt -alias "$filename" -file "$filename"
+        keytool -import -trustcacerts -keystore $KEYSTORE_PATH -storepass changeit -noprompt -alias "$filename" -file "$filename"
     done;
 fi
 
