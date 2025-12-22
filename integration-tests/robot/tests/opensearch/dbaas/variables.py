1766f0eb3b862433eeab78fabbeedc8d84654a40
Artem Badeinov <51969741+Gravelord@users.noreply.github.com>
2025-12-23 01:02:56 +0300

    fix: remove variables.py

--- integration-tests/robot/tests/opensearch/dbaas/variables.py
+++
@@ -1,2 +0,0 @@
-body = {'data': {'password': 'UUEtZ29vZC1wYXNzd29yZDEhLUFU', 'username': 'T3BlbnNlYXJjaC1hZG1pbjEhLUFU'}}
-body_default = {'data': {'password': 'Um9vdDEyMzQj', 'username': 'YWRtaW4='}}
